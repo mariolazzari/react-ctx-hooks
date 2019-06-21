0005eb296276bdf07f6c179862437d415e10b202
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-21 22:46:52 +0200

    Done lesson 4

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return (
       <div>
-        <nav>
+        <nav style={{ background: theme.ui, color: theme.syntax }}>
           <h1>Context app</h1>
           <ul>
             <li>Home</li>
--- contextapp/src/contexts/ThemeContext.js
+++ contextapp/src/contexts/ThemeContext.js
@@ -8,9 +8,13 @@ class ThemContextProvider extends Component {
     dark: { syntax: "#ddd", ui: "#333", bg: "#555" }
   };
 
+  toggleTheme = this.setState({ isLightTheme: !this.state.isLightTheme });
+
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
