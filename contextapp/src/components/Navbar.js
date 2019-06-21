157cc338b425080a14afa3442eef452fc52f6dbc
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-21 22:14:47 +0200

    Done lesson 1

---
+++ contextapp/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <h1>Context app</h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
