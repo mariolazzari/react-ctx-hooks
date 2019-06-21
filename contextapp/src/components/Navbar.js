b47f2ebcf0228b99c54aedcf0f3444095653c250
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-21 22:52:36 +0200

    Done lesson 5

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -10,14 +10,22 @@ class Navbar extends Component {
 
     return (
       <div>
-        <nav style={{ background: theme.ui, color: theme.syntax }}>
-          <h1>Context app</h1>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
+        <ThemeContext.Consumer>
+          {context => {
+            const { isLightTheme, light, dark } = context;
+            const theme = isLightTheme ? light : dark;
+            return (
+              <nav style={{ background: theme.ui, color: theme.syntax }}>
+                <h1>Context app</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            );
+          }}
+        </ThemeContext.Consumer>
       </div>
     );
   }
