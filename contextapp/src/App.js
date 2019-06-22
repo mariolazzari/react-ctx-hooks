7a96d418c08baf231a8bff715fd6ab18e2e0ae07
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-22 09:32:10 +0200

    Done lesson 8

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
+
+// context
+import AuthContextProvider from "./contexts/AuthContext";
+import ThemContextProvider from "./contexts/ThemeContext";
+
+// components
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
-import ThemContextProvider from "./contexts/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <ThemContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemContextProvider>
     </div>
   );
-}
+};
 
 export default App;
