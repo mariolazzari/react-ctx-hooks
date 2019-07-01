e9db312a0f0cbc590082df992ed515108ebb278c
Mario Lazzari <mario.lazzari@gmail.com>
2019-07-01 20:32:27 +0200

    Done lesson 19

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from "react";
-import uuid from "uuid";
+import uuid from "uuid/v1";
 
 export const BookContext = createContext();
 
@@ -9,13 +9,10 @@ const BookContextProvider = props => {
     { title: "the final empire", author: "brandon sanderson", id: 2 }
   ]);
 
-  const addBook = (title, author) => {
-    setBooks(...books, { title, author, id: uuid() });
-  };
+  const addBook = (title, author) =>
+    setBooks([...books, { title, author, id: uuid() }]);
 
-  const removeBook = id => {
-    setBooks(books.filter(book => book.id !== id));
-  };
+  const removeBook = id => setBooks(books.filter(book => book.id !== id));
 
   return (
     <BookContext.Provider value={{ books, addBook, removeBook }}>
